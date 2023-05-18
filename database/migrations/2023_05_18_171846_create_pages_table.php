4484acab97011f13d485e3ce826b12b2dfadb634
Miguel Ceballos <mceballosleonides@gmail.com>
2023-05-18 11:22:06 -0600

    Create Migration, Model and Controller to Pages table

--- database/migrations/2023_05_18_171846_create_pages_table.php
+++ database/migrations/2023_05_18_171846_create_pages_table.php
@@ -4,19 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('pages', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('state_id');
-            $table->string('task');
-            $table->string('comment');
-            $table->foreignId('page_id');
+            $table->string('page');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('pages');
     }
 };
