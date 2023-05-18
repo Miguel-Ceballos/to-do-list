945701bd952d5386caf93aa6306d76a8f36abdd4
Miguel Ceballos <mceballosleonides@gmail.com>
2023-05-18 11:16:55 -0600

    Create Migration, Model and Controller to table states

--- database/migrations/2023_05_18_171239_create_states_table.php
+++ database/migrations/2023_05_18_171239_create_states_table.php
@@ -4,19 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('states', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('state_id');
-            $table->string('body');
-            $table->string('comment');
-            $table->foreignId('page_id');
+            $table->string('state');
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('states');
     }
 };
